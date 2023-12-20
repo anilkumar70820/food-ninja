078e7fb385bc9bf90f5720a1a41ee8fa1ef49f0b
anilkumar70820 <ak9565191@gmail.com>
2023-12-20 15:11:26 +0530

    correct subscribe section

--- src/components/AllBlogs.jsx
+++ src/components/AllBlogs.jsx
@@ -134,46 +134,59 @@ const AllBlogs = () => {
             </div>
           </div>
           </Link>
-         
         </div>
-        <div className="flex lg:mt-[266px] md:mt-[180px] sm:mt-20 mt-12 mb-10 md:justify-start justify-center">
-          <div className="flex lg:gap-20 gap-6 md:flex-row flex-col">
-            <div className="max-w-[382px] px-[27px] py-[38px] bg-violet rounded-[5px]">
-              <h3 className="text-white text-center font-merriweather text-xmd font-black leading-[160%] max-w-[182px] mx-auto mb-[13px]">
-                Subscribe To Our Newsletter
-              </h3>
-              <p className="font-roboto text-sm font-medium text-Alice-Blue leading-[160%] mb-[17px] text-center">
-                Get weekly food news, articles, and videos delivered to your
-                inbox.
-              </p>
-              <form>
-                <div className="flex justify-center">
-                  <input
-                    required
-                    className="px-[15px] py-[11px] text-sm font-medium leading-[160%] font-roboto text-Slate-Gray bg-white rounded-[3px] max-w-[288px] w-full mb-[23px]  mx-auto outline-none"
-                    type="email"
-                    placeholder="Email"
-                  />
-                </div>
-                <button className="hover:bg-white hover:border-violet hover:text-violet transition-all duration-300 border border-solid border-transparent text-white font-roboto text-sm font-medium leading-[160%] bg-Midnight-Blue rounded-[3px] ms-[22px] px-[18px] py-[9px]">
-                  Sign Up
-                </button>
-              </form>
-            </div>
-            <div className="max-w-[320px]">
-              <h4 className="font-roboto text-2xsm font-medium leading-[160%] text-black mb-[27px]">
-                POPULAR POSTS
-              </h4>
-              <p className="mb-[22px] font-merriweather text-2xsm font-normal leading-[160%] text-black">
-                How To Have Your Cake and Eat It Too: The Way of The Chicken Man
-              </p>
-              <p className="mb-[22px] font-merriweather text-2xsm font-normal leading-[160%] text-black">
-                My Grandma’s 30-year-old Recipe
-              </p>
-              <p className="font-merriweather text-2xsm font-normal leading-[160%] text-black">
-                What I learned about cooking from Ratatoulie
-              </p>
-            </div>
+      </div>
+      <div className="max-w-[1440px] mx-auto 2xl:px-0 px-3 lg:mt-[266px] md:mt-[180px] sm:mt-20 mt-12 mb-10">
+        <div className="flex gap-6 flex-wrap xl:ms-[170px] xl:max-w-[1182px] justify-between ">
+          <div className="max-w-[382px] px-[27px] py-[38px] bg-violet rounded-[5px]">
+            <h3 className="text-white text-center font-merriweather text-xmd font-black leading-[160%] max-w-[182px] mx-auto mb-[13px]">
+              Subscribe To Our Newsletter
+            </h3>
+            <p className="font-roboto text-sm font-medium text-Alice-Blue leading-[160%] mb-[17px] text-center">
+              Get weekly food news, articles, and videos delivered to your
+              inbox.
+            </p>
+            <form>
+              <div className="flex justify-center">
+                <input
+                  required
+                  className="px-[15px] py-[11px] text-sm font-medium leading-[160%] font-roboto text-Slate-Gray bg-white rounded-[3px] max-w-[288px] w-full mb-[23px] outline-none"
+                  type="email"
+                  placeholder="Email"
+                />
+              </div>
+              <button className="hover:bg-white hover:border-violet hover:text-violet transition-all duration-300 border border-solid border-transparent text-white font-roboto text-sm font-medium leading-[160%] bg-Midnight-Blue rounded-[3px] ms-[22px] px-[18px] py-[9px]">
+                Sign Up
+              </button>
+            </form>
+          </div>
+          <div className="max-w-[320px]">
+            <h4 className="font-roboto text-2xsm font-medium leading-[160%] text-black mb-[27px]">
+              POPULAR POSTS
+            </h4>
+            <p className="mb-[22px] font-merriweather text-2xsm font-normal leading-[160%] text-black">
+              How To Have Your Cake and Eat It Too: The Way of The Chicken Man
+            </p>
+            <p className="mb-[22px] font-merriweather text-2xsm font-normal leading-[160%] text-black">
+              My Grandma’s 30-year-old Recipe
+            </p>
+            <p className="font-merriweather text-2xsm font-normal leading-[160%] text-black">
+              What I learned about cooking from Ratatoulie
+            </p>
+          </div>
+          <div className="max-w-[320px]">
+            <h4 className="font-roboto text-2xsm font-medium leading-[160%] text-black mb-[27px]">
+              Recent Posts
+            </h4>
+            <p className="mb-[22px] font-merriweather text-2xsm font-normal leading-[160%] text-black">
+              How To Have Your Cake and Eat It Too: The Way of The Chicken Man
+            </p>
+            <p className="mb-[22px] font-merriweather text-2xsm font-normal leading-[160%] text-black">
+              My Grandma’s 30-year-old Recipe
+            </p>
+            <p className="font-merriweather text-2xsm font-normal leading-[160%] text-black">
+              What I learned about cooking from Ratatoulie
+            </p>
           </div>
         </div>
       </div>
